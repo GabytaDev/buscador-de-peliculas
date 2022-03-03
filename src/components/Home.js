b93ffcdeb5dfae956189ff13b026d897e7ca6e6f
GabytaDev <gabyta.loyola@gmail.com>
2022-03-03 17:33:44 -0300

    agrego fetch y map a home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+//https://api.themoviedb.org/3/movie/550?api_key=90d7b1e3c47854073e093a7f557291b5
+import { Link } from "react-router-dom";
+
+
 
 const Home = ()=>{
+
+    const [peliculasPopulares, setPeliculasPopulares]= useState ([])
+    useEffect(()=>{
+      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=90d7b1e3c47854073e093a7f557291b5&language=es`)
+      .then(res => res.json())
+      .then(data => setPeliculasPopulares(data.results))
+    },[])
+
     return (
         <div>
-           <h2> home</h2>
+           <h2>Peliculas Populares</h2>
+            <div className="Card-movie">
+                {peliculasPopulares.map(pelicula => (
+
+                   <Link to={`/movie/${pelicula.id}`} key={pelicula.id}>
+                     <h2>{pelicula.original_title}</h2>
+                     <img src={`https://image.tmdb.org/t/p/w200/${pelicula.poster_path}`}></img>
+                   </Link> 
+                ))}
+            </div>
         </div>
     )
 }
