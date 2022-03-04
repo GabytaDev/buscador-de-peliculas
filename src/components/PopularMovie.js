c90596190ba61065181e98e27cfd33c26f05030f
GabytaDev <gabyta.loyola@gmail.com>
2022-03-04 16:58:17 -0300

    continuo estilos a componente PopularMovies

--- src/components/PopularMovie.js
+++ src/components/PopularMovie.js
@@ -1,32 +1,35 @@
 import { useState, useEffect } from "react";
 //https://api.themoviedb.org/3/movie/550?api_key=90d7b1e3c47854073e093a7f557291b5
 import { Link } from "react-router-dom";
+import { IoIosArrowDroprightCircle as Flecha } from "react-icons/io";
 
 
 const PopularMovie = () => {
 
     const [peliculasPopulares, setPeliculasPopulares] = useState([])
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=90d7b1e3c47854073e093a7f557291b5&language=es`)
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=90d7b1e3c47854073e093a7f557291b5&language=es&page=1`)
             .then(res => res.json())
             .then(data => setPeliculasPopulares(data.results))
     }, [])
 
     return (
-        <div>
+        <div className="box-container">
             <h2 className="titulo-container">Peliculas Populares</h2>
             <div className="contenedor-listado">
                 {peliculasPopulares.map(pelicula => (
-
-                    <Link  to={`/movie/${pelicula.id}`} key={pelicula.id}>
-                        <ul className="listado">
+                    <Link  to={`/movie/${pelicula.id}`} key={pelicula.id} className="listado">
+                        <ul className="listado-ul">
                             <li className="item-listado">
                                 <img src={`https://image.tmdb.org/t/p/w200/${pelicula.poster_path}`} className="contenedor-imagen-listado"></img>
-                                <h3>{pelicula.original_title}</h3>  
-                            </li>
+                                <p>{pelicula.original_title}</p>  
+                            </li>    
                         </ul>
+                        <Flecha className="icono-flecha-detalle"/>
                     </Link>
+                    
                 ))}
+                
             </div>
         </div>
     )
