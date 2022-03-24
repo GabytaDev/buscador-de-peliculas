113da33c04dd0aaa577df221575cdc85aac04b53
GabytaDev <gabyta.loyola@gmail.com>
2022-03-24 12:38:40 -0300

    arreglo fetch resultado busqueda

--- src/utils/Variables.js
+++ src/utils/Variables.js
@@ -1,5 +1,10 @@
 export const urlBase = "https://api.themoviedb.org/3/";
 export const apiKey = "api_key=90d7b1e3c47854073e093a7f557291b5";
 export const search = "search";
+// export const queryParamPagina = '&page=';
+
+// export const parametrosFetch = (categoria,tipo, busqueda) =>
+// busqueda ? `${categoria}/${tipo}/${busqueda}` : `${tipo}/${categoria}` ;
+
 export const parametrosFetch = (categoria,tipo) =>
 `${categoria}/${tipo}`;
