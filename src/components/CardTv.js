90a42acf30e75a86ac44033b80b8e8b5360d641c
GabytaDev <gabyta.loyola@gmail.com>
2022-03-28 19:32:58 -0300

    agrego imagen not foun a cardtv

--- src/components/CardTv.js
+++ src/components/CardTv.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import portadaNotfound from "../images/portadaNotfound.jpeg";
 import "../styles/_Card.scss"
 
 const CardTv = ({ peliculas}) => {
@@ -12,10 +12,11 @@ const CardTv = ({ peliculas}) => {
                     <div className="unit-card">
                         <p className="text-detalle">{serie.name}</p>
                         <div className="content-img">
+                        {serie.poster_path ?
                             <img src={`https://image.tmdb.org/t/p/w200/${serie.poster_path}`}
-
                                 alt={serie.name}>
-                            </img>
+                            </img> 
+                            : <img src={portadaNotfound}></img>}
                         </div>
                     </div>
                 </Link>
