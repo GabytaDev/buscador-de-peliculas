15fd7de1d529935e614da0051f6a979ab26fe0a9
GabytaDev <gabyta.loyola@gmail.com>
2022-03-28 11:02:54 -0300

    agrego estilos a tv detalle

--- src/components/TvDetalle.js
+++ src/components/TvDetalle.js
@@ -1,31 +1,62 @@
 import "../styles/_TvDetalle.scss";
-import {urlBase, apiKey} from "../utils/Variables" 
+import { urlBase, apiKey } from "../utils/Variables"
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-const TvDetalle = ()=>{
+const TvDetalle = () => {
     const params = useParams()
     const [series, setSeries] = useState([])
-   
+
     useEffect(() => {
         fetch(`${urlBase}/tv/${params.idtv}?${apiKey}&language=es-ES`)
             .then(res => res.json())
             .then(data => setSeries(data))
-            console.log(series)
+        console.log(series)
     }, [])
 
-  
+
     return (
-        <div >
-            <div className="container-imagen-tv">
-            <img src={`https://image.tmdb.org/t/p/original${series.backdrop_path}`}></img>
+        <div>
+            <div className="container-background-tv">
+                <img src={`https://image.tmdb.org/t/p/original${series.backdrop_path}`} className="img-fondo-tv"></img>
+
+                <section className="container-tv">
+                    <div className="content-img-tv">
+                        <img src={`https://image.tmdb.org/t/p/w300${series.poster_path}`} alt={series.name}></img>
+                    </div>
+                    <article className="content-info-tv">
+                        <h2 className="name-tv">{series.name}</h2>
+                        <h4 className="subtitle-tv">General:</h4>
+                        <p className="text-tv">{series.overview}</p>
+                        <h4 className="subtitle-tv">Puntuación promedio</h4>
+                        <p>{series.vote_average}</p>
+
+                        <div className="text-tv">
+                            <h4 className="subtitle-tv">Géneros:</h4>
+                            {series?.genres?.map(genero => (
+                                <span className="text-tv"> {genero.name}</span>
+                            ))}
+                        </div>
+
+                        <div className="text-tv">
+                            <h4 className="subtitle-tv">Temporadas:</h4>
+                            {series?.seasons?.map(temporada => (
+                                <span className="text-tv">{temporada.season_number}</span>
+                            ))}
+                        </div>
+
+                        <div className="text-tv">
+                            <h4 className="subtitle-tv">Episodios:</h4>
+                            {series?.seasons?.map(temporada => (
+                                <span className="text-tv"> {temporada.episode_count}</span>
+                            ))}
+                        </div>
+
+                    </article>
+                </section>
             </div>
-            <article>
-            <p>{series.name}</p>
-            <p>{series.overview}</p>
-            <img src={`https://image.tmdb.org/t/p/w300${series.poster_path}`}></img>
-            </article>
-            
+
+
 
         </div>
     )
