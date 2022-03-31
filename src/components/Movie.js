dae9a27edcd04534882a3f7fcb195541dd607a70
GabytaDev <gabyta.loyola@gmail.com>
2022-03-31 10:43:14 -0300

    acomodo identado de codigo

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,51 +5,51 @@ import { TiArrowRightOutline as FlechaVerMas } from "react-icons/ti";
 import { Link } from "react-router-dom";
 import "../styles/_Card.scss";
 
-const Movie = ()=>{
-    const { peliculas: moviesPopular } = useFetchMovies("movie", "popular",1);
-    const { peliculas: moviesTopRated } = useFetchMovies("movie", "top_rated",1);
-    const { peliculas: moviesUpComing } = useFetchMovies("movie", "upcoming",1);
-    const { peliculas: moviesNowPlaying } = useFetchMovies("movie", "now_playing",1);
-    
+const Movie = () => {
+    const { peliculas: moviesPopular } = useFetchMovies("movie", "popular", 1);
+    const { peliculas: moviesTopRated } = useFetchMovies("movie", "top_rated", 1);
+    const { peliculas: moviesUpComing } = useFetchMovies("movie", "upcoming", 1);
+    const { peliculas: moviesNowPlaying } = useFetchMovies("movie", "now_playing", 1);
+
 
     return (
         <div>
             <div className="container-titulo-link">
-            <h2 className="titulo-cards">Peliculas populares</h2>
-            <Link to="popular">
-                <FlechaVerMas className="flecha-ver-mas"/>
-            </Link>
+                <h2 className="titulo-cards">Peliculas populares</h2>
+                <Link to="popular">
+                    <FlechaVerMas className="flecha-ver-mas" />
+                </Link>
             </div>
-           <CardMovie
-           peliculas={cortarArray(moviesPopular)}
-           />
-            
-            <div className="container-titulo-link">
-            <h2 className="titulo-cards">Peliculas con mejores criticas</h2>
-            <Link to="top_rated">
-                <FlechaVerMas className="flecha-ver-mas"/>
-            </Link>
-            </div>
-           <CardMovie
-           peliculas={cortarArray(moviesTopRated)}/>
+            <CardMovie
+                peliculas={cortarArray(moviesPopular)}
+            />
 
             <div className="container-titulo-link">
-            <h2 className="titulo-cards">Peliculas a estrenar</h2>
-            <Link to="upcoming">
-                <FlechaVerMas className="flecha-ver-mas"/>
-            </Link>
+                <h2 className="titulo-cards">Peliculas con mejores criticas</h2>
+                <Link to="top_rated">
+                    <FlechaVerMas className="flecha-ver-mas" />
+                </Link>
             </div>
-           <CardMovie
-           peliculas={cortarArray(moviesUpComing)}/>
+            <CardMovie
+                peliculas={cortarArray(moviesTopRated)} />
 
-<div className="container-titulo-link">
-            <h2 className="titulo-cards">Peliculas en el cine</h2>
-            <Link to="now_playing">
-                <FlechaVerMas className="flecha-ver-mas"/>
-            </Link>
+            <div className="container-titulo-link">
+                <h2 className="titulo-cards">Peliculas a estrenar</h2>
+                <Link to="upcoming">
+                    <FlechaVerMas className="flecha-ver-mas" />
+                </Link>
             </div>
-           <CardMovie
-           peliculas={cortarArray(moviesNowPlaying)}/>
+            <CardMovie
+                peliculas={cortarArray(moviesUpComing)} />
+
+            <div className="container-titulo-link">
+                <h2 className="titulo-cards">Peliculas en el cine</h2>
+                <Link to="now_playing">
+                    <FlechaVerMas className="flecha-ver-mas" />
+                </Link>
+            </div>
+            <CardMovie
+                peliculas={cortarArray(moviesNowPlaying)} />
         </div>
     )
 }
