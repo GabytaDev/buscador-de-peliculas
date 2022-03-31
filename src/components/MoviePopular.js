cc9460b34b427c5dbcd9a4ff7616363bbad87ec2
GabytaDev <gabyta.loyola@gmail.com>
2022-03-31 10:08:31 -0300

    agrego estilo a moviepopular

--- src/components/MoviePopular.js
+++ src/components/MoviePopular.js
@@ -2,15 +2,17 @@ import useFetchMovies from '../hooks/useFetchMovies';
 import CardMovie from './CardMovie';
 import "../styles/_Card.scss";
 
-const MoviePopular = ()=>{
+const MoviePopular = () => {
 
     const { peliculas: moviesPopular } = useFetchMovies("movie", "popular");
 
     return (
-        <div> 
-          <h2 className="titulo-cards">Peliculas populares</h2>
+        <div>
+            <div className='container-titulo-link'>
+            <h2 className="titulo-cards">Peliculas populares</h2>
+            </div>
             <CardMovie
-            peliculas={moviesPopular}
+                peliculas={moviesPopular}
             />
         </div>
     )
