4db3d8ec012d7cb0958475e4a9cd979982f329d1
GabytaDev <gabyta.loyola@gmail.com>
2022-04-04 11:11:45 -0300

    agrego titulos a los componentes TVonair, tvPopulares y Tvranking

--- src/components/TvOnAir.js
+++ src/components/TvOnAir.js
@@ -2,12 +2,17 @@ import useFetchMovies from "../hooks/useFetchMovies";
 import usePaginado from '../hooks/usePaginado';
 import CardTv from "./CardTv";
 import Paginado from './Paginado';
+import "../styles/_Card.scss";
+
 const TvOnAir = ()=>{
     const {page, handleClickPrimerPagina, handleClickPrev, handleClickNext,handleClickUltimaPagina} = usePaginado()
     const {peliculas: seriesOnTheAir, totalPages} = useFetchMovies("tv", "on_the_air", page);
     
     return(
         <div>
+             <div className='container-titulo-link'>
+            <h2 className="titulo-cards">Series al aire</h2>
+            </div>
             <CardTv
                 peliculas={seriesOnTheAir}
             />
