716234cf8dfdb608805d7f644d68aadc2ae4d99f
GabytaDev <gabyta.loyola@gmail.com>
2022-04-06 08:54:38 -0300

    agrego estilos y texto

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -2,13 +2,20 @@
 import "../styles/_ErrorPage.scss"
 import page404 from "../images/page404.png";
 
-const ErrorPage = ()=>{
+const ErrorPage = () => {
 
- return (
-     <div className="content-img">
-       <img src={page404} className="error-page"></img> 
-     </div>
- )
+    return (
+        <div className="content">
+            <div className="content-text">
+                <h3 className="title">Error 404: página no encontrada</h3>
+                <p className="text">La página que intentas buscar no se encuentra</p>
+                <button>Volver al inicio</button>
+            </div>
+            <div className="content-img">
+                <img src={page404} className="img-error" alt="pagina no encontrada"></img>
+            </div>
+        </div>
+    )
 }
 
 export default ErrorPage;
