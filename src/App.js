d07a21ba8cdb623d6036b2acc279b92df67eb1c7
GabytaDev <gabyta.loyola@gmail.com>
2022-04-08 09:04:23 -0300

    arreglo detalle de busqueda

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import MovieNow from './components/MovieNow';
 import Footer from './components/Footer';
 import ErrorPage from './components/ErrorPage'
 
+
 const App = ()=>{
   return (
     <div>
@@ -31,67 +32,17 @@ const App = ()=>{
         <Route path="buscar/:resultado" element={<ResultadoBusqueda/>}/> 
         <Route path="/" element={<Home/>} />
         <Route path="movie/:idPelicula" element={<MovieDetalle/>}/>
-        <Route path="/buscar/multi/:mediatype" element={<Card/>}/>
-
         <Route path="/movie" element={<Movie/>}/>
         <Route path="movie/popular" element={<MoviePopular/>}/> 
         <Route path="/movie/top_rated" element={<MovieTopRated/>}/>
         <Route path="/movie/upcoming" element={<MovieUpComing/>}/>
         <Route path="/movie/now_playing" element={<MovieNow/>}/>
-
         <Route path="/tv" element={<Tv/>}/>
         <Route path="/tv/:idtv" element={<TvDetalle/>}/>
         <Route path="/tv/popular" element={<TvPopular/>} />
         <Route path="/tv/toprated" element={<TvRanking/>} />
         <Route path="/tv/ontheair" element={<TvOnAir/>}/>
         <Route path="*" element={<ErrorPage/>} />
-        
- {/* 
-         {/*home --> peliculas tendencias  
-        <Route path="movie/:trending/page/:numpage" element={<TrendingMovie/>}/> 
-        {/*home --> peliculas tendencias--> click en pelicula  
-        <Route path="/movie/:idPelicula/info" element={<infoMovie/>}/>
-        <Route  path="/movie/:idPelicula/cast/" element={<RepartoMovie/>}/>
-        <Route  path="/movie/:idPelicula/videos/" element={<VideoMovie/>}/>
-        <Route  path="/movie/:idPelicula/similars/" element={<SimilarMovie/>}/>
-
-        {/* navbar pelis --> peliculas populares 
-        
-        <Route path="/movie/:idPelicula/info" element={<infoMovie/>}/>
-        <Route  path="/movie/:idPelicula/cast/" element={<RepartoMovie/>}/>
-        <Route  path="/movie/:idPelicula/videos/" element={<VideoMovie/>}/>
-        <Route  path="/movie/:idPelicula/videos/" element={<VideoMovie/>}/>
-
-        {/* desde navbar ---> peliculas con mejores criticas 
-        <Route path="/movie/:top_rated/page/:numpage" element={<RankingMovie/>}/>
-        {/* desde navbar ---> peliculas a estrenar 
-        <Route path="/movie/:upcoming/page/:numberpage" element={<UpcomingMovie/>}/>
-        {/* desde navbar ---> peliculas en cine 
-        <Route path="/movie/:now_playing/page/:numberpage" element={<NowMovie/>}/>
-       
-       
-        {/*  desde navbar ---> tendencias 
-        <Route path="/series/:trending/page/:numpage"/>
-        
-        
-        <Route path="/lanzamientos" element={<Lanzamientos/>}/>
-        <Route path="/:idPelicula/" element={<DetallePelicula/>}/>
-        <Route path="/:idPelicula/:reparto" element={<Reparto/>}/>
-
-        {/* tv 
-        {/* home--> tv tendencias 
-        <Route path="tv/:trending/page/:numpage" element={<tvTendencia/>}/>
-       
-        <Route path="/tv/:top_rated/page/:numpage" element={<RankingTv/>}/>
-        
-        
-        <Route  path="/tv/:idPelicula/temporada/:numTemporada" element={<TemporadaTv/>}/>
-        <Route  path="/tv/:idPelicula/cast/" element={<RepartoTv/>}/>
-        <Route  path="/tv/:idPelicula/similar/" element={<SimilarTv/>}/>
-
-        
-        */}
-
       </Routes>
         <Footer/>
       </BrowserRouter>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
-import portadaNotfound from "../images/portadaNotfound.jpeg"
+import portadaNotfound from "../images/portadaNotfound.jpeg";
+import "../styles/_Card.scss";
 
-const Card = ({resultado, mediaType,})=>{
- return (
-        <Link to={`/buscar/multi/${mediaType}`} className="card">
-             <p className="text-detalle">
-                {resultado.title ? resultado.title : resultado.name}
-            </p>
-            <div className="content-img">
-            {resultado.poster_path?
+const Card = ({ resultado, mediaType }) => {
+  return (
+    <div>
+      <Link to={`../${mediaType}/${resultado.id}`} className="card" key={resultado.id}>
+        <div className="unit-card">
+          <p className="text-detalle">
+          {resultado.title ? resultado.title : resultado.name}
+          </p>
+
+          <div>
+            {resultado.poster_path ?
               <img src={`https://image.tmdb.org/t/p/w200/${resultado.poster_path}`}
-              alt="poster de pelicula"></img>
-              : <img src={portadaNotfound} className="not-img-portada"></img>} 
-            </div>
-         </Link>
-        
-    )}
+                alt={resultado.title}>
+              </img>
+              : <img src={portadaNotfound} className="content-img"></img>}
+          </div>
+        </div>
 
+      </Link>
+    </div>
+
+  )
+}
 export default Card;
